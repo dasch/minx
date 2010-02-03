80c65f406410718bd53bd371feba691bb6da2175
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-02-03 13:37:29 +0100

    Nest the classes inside the Minx module

--- lib/minx/channel.rb
+++ lib/minx/channel.rb
@@ -1,32 +1,34 @@
 
-# A Channel is used to transmit messages between processes in a synchronized
-# manner.
-class Minx::Channel
-  def initialize
-    @readers = []
-    @writers = []
-  end
+module Minx
+  # A Channel is used to transmit messages between processes in a synchronized
+  # manner.
+  class Channel
+    def initialize
+      @readers = []
+      @writers = []
+    end
 
-  # Write a message to the channel.
-  #
-  # If no readers are waiting, the calling process will block until one comes
-  # along.
-  #
-  # @param message the message to be transmitted
-  # @return [nil]
-  def send(message)
-    @readers.shift.resume(message)
-    return nil
-  end
+    # Write a message to the channel.
+    #
+    # If no readers are waiting, the calling process will block until one comes
+    # along.
+    #
+    # @param message the message to be transmitted
+    # @return [nil]
+    def send(message)
+      @readers.shift.resume(message)
+      return nil
+    end
 
-  # Read a message off the channel.
-  #
-  # If no messages have been written to the channel, the calling process will
-  # block, only resuming when a write occurs.
-  #
-  # @return a message
-  def receive
-    @readers << Fiber.current
-    Fiber.yield
+    # Read a message off the channel.
+    #
+    # If no messages have been written to the channel, the calling process will
+    # block, only resuming when a write occurs.
+    #
+    # @return a message
+    def receive
+      @readers << Fiber.current
+      Fiber.yield
+    end
   end
 end
--- lib/minx/process.rb
+++ lib/minx/process.rb
@@ -1,15 +1,17 @@
 
-class Minx::Process
-  def initialize(&block)
-    raise ArgumentError unless block_given?
-    @fiber = Fiber.new { block.call }
-  end
+module Minx
+  class Process
+    def initialize(&block)
+      raise ArgumentError unless block_given?
+      @fiber = Fiber.new { block.call }
+    end
 
-  # Spawn the process.
-  #
-  # The process will immediately take over execution, and the current
-  # fiber will yield.
-  def spawn
-    @fiber.resume
+    # Spawn the process.
+    #
+    # The process will immediately take over execution, and the current
+    # fiber will yield.
+    def spawn
+      @fiber.resume
+    end
   end
 end
