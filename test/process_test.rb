bdb997c075b08151d562ff3118a053e928d98029
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-02-07 17:30:00 +0100

    Add failing test for scheduling

--- test/process_test.rb
+++ test/process_test.rb
@@ -21,4 +21,19 @@ class ProcessTest < Test::Unit::TestCase
       assert_equal "foo", @data
     end
   end
+
+  context "A Minx process that yields initially" do
+    setup do
+      @process = Minx::Process.new { Minx.yield; @value = 42 }
+    end
+
+    should "be rescheduled and resumed" do
+      Minx.spawn do
+        @process.spawn
+        Minx.yield
+      end
+
+      assert_equal 42, @value
+    end
+  end
 end
