75f1cda8e6283b2093e7632147211954eca39566
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-02-07 15:30:34 +0100

    Document Minx.select

--- lib/minx.rb
+++ lib/minx.rb
@@ -17,6 +17,16 @@ module Minx
     Fiber.yield(*args)
   end
 
+  # Select from a list of channels.
+  #
+  # The channels will be enumerated in order; the first one carrying a message
+  # will be picked, and the message will be returned.
+  #
+  # If none of the channels are readable, the calling process will yield until
+  # a channel is written to.
+  #
+  # @param choices [Channel] the channels to be selected among
+  # @return the first message received from any of the channels
   def self.select(*choices)
     # If a choice is readable, just receive from that one.
     choices.each do |choice|
