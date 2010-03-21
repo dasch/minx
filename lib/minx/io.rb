47e1d818ccda864a6f00cdbb05bda276dc982383
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-03-21 15:00:36 +0100

    Add simple non-blocking socket support

--- lib/minx/io.rb
+++ lib/minx/io.rb
@@ -6,16 +6,38 @@ module Minx
     end
 
     def readline
-      # Yield until the IO object is ready for reading.
-      Minx.yield until select([@io])
-
       @io.readline
+    end
+
+    def write(str)
+      @io.write(str)
+    end
+
+    def read
+      @io.read
     end
   end
 
   class File < IO
     def initialize(filename)
       super(::File.open(filename))
+    end
+  end
+
+  class Socket < IO
+    def initialize(*args)
+      require 'socket'
+
+      @socket = ::Socket::Socket.new(*args)
+      super(@socket)
+    end
+
+    def connect(sockaddr)
+      @socket.connect_nonblock(sockaddr)
+    rescue Errno::EINPROGRESS
+      Minx.yield until ::IO.select([], [@socket], nil, 0)
+
+      @socket.connect_nonblock(sockaddr)
     end
   end
 end
