0cd863670adfce7641e7c8bd6a6d5fffb5bfa58e
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-04-14 19:00:25 +0200

    Add memory leak test

---
+++ examples/leak.rb
@@ -0,0 +1,14 @@
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'minx'
+
+N = ARGV[0] ? Integer(ARGV[0]) : 100000
+
+N.times { Minx.spawn { Minx.yield } }
+
+GC.start
+
+count = ObjectSpace.each_object(Minx::Process) { }
+
+puts "Number of Process instances: #{count}"
