653d51309b0ccbc2ccc5313cda4eb436bd511028
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-05-24 18:41:19 +0200

    Fix Minx.filter tests

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -24,15 +24,13 @@ class UtilitiesTest < Test::Unit::TestCase
       @process = Minx.filter(@chan1, @chan2) {|i| i % 2 == 0 }
     end
 
-    should "map messages from the input to the output channel" do
-      p2 = Minx.spawn do
-        assert_equal 4, @chan2.read
-        assert_equal 18, @chan2.read
-      end
-
-      p1 = Minx.spawn do
+    should "filter messages" do
+      Minx.spawn do
         @chan1 << 7 << 4 << 18
       end
+
+      assert_equal 4,  @chan2.read
+      assert_equal 18, @chan2.read
     end
   end
 end
