b73a4de91e6458f1a5e80b8c57447c0f4b28e24a
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-05-27 17:47:21 +0200

    Improve the ring example

--- examples/ring.rb
+++ examples/ring.rb
@@ -3,16 +3,18 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'minx'
 
-N = 1000
-M = 5000
+# Number of rounds.
+M = ARGV[0] ? Integer(ARGV[0]) : 1001
+
+# Number of elements.
+N = ARGV[1] ? Integer(ARGV[1]) : 64
 
 def node(input, id)
   output = Minx.channel
   Minx.spawn do
-    input.each do |message|
-      message += 1
+    input.each do |i|
       #puts "[#{id}] => #{message.inspect}"
-      output.write(message)
+      output.write(i + 1)
       #puts "[#{id}] DONE"
     end
   end
@@ -21,12 +23,15 @@ end
 
 FIRST = Minx.channel
 
-LAST = (0...N).inject(FIRST) {|chan, i| node(chan, i) }
+LAST = (0...N).inject(FIRST) {|chan, id| node(chan, id) }
 
-FIRST.write(0)
+i = 0
 
-(M - 1).times do |i|
-  #puts "==== ROUND #{i} ===="
-  message = LAST.read
-  FIRST.write(message)
+FIRST.write(i)
+
+M.times do
+  i = LAST.read
+  FIRST.write(i)
 end
+
+puts "Result: #{i}"
