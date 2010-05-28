ea4b003cc48c0d6e2f28ff3187c0c8bb385717fc
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-05-28 18:30:21 +0200

    Improve memleak example

--- examples/leak.rb
+++ examples/leak.rb
@@ -5,10 +5,12 @@ require 'minx'
 
 N = ARGV[0] ? Integer(ARGV[0]) : 100000
 
-N.times { Minx.spawn { } }
+N.times { Minx.spawn { Minx.channel.read } }
 
 GC.start
 
-count = ObjectSpace.each_object(Minx::Process) { }
+processes = ObjectSpace.each_object(Minx::Process) { }
+channels = ObjectSpace.each_object(Minx::Channel) { }
 
-puts "Number of Process instances: #{count}"
+puts "Number of Process instances: #{processes}"
+puts "Number of Channel instances: #{channels}"
