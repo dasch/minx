5f3b12b85cd22485aa9c22463ea12cacf991b9a2
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-06-02 12:50:59 +0200

    Add Sieve of Eratosthenes example

---
+++ examples/sieve.rb
@@ -0,0 +1,41 @@
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'minx'
+
+def generate(range, cout)
+  Minx.spawn { range.each {|i| cout.write(i) } }
+end
+
+def printer(cin)
+  Minx.spawn { cin.each {|i| puts i } }
+end
+
+def worker(cin, cout)
+  Minx.spawn do
+    # Fetch the first prime number.
+    prime = cin.read
+
+    # Pass it on.
+    cout.write(prime)
+
+    # construct a child process.
+    chan = Minx.channel
+
+    worker(chan, cout)
+
+    cin.each do |new_prime|
+      if new_prime % prime != 0
+        chan.write(new_prime) 
+      end
+    end
+  end
+end
+
+JOBS    = Minx.channel
+RESULTS = Minx.channel
+
+printer(RESULTS)
+worker(JOBS, RESULTS)
+
+Minx.join(generate(2..800, JOBS))
